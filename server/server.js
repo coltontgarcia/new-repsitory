54c437d2ce9d0dc00e70b1623452ac908f8e28c7
Colton Garcia <coltontgarcia@gmail.com>
2022-03-16 12:20:53 -0600

    css stuffs

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
+const express = require('express')
+const path = require('path')
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../index.html"))
+app.get('/',function(req,res) {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+app.get("/styles", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.css"));
+});
+const port = process.env.PORT || 4005
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
 })
-
-const port = process.env.PORT
